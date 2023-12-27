85535621faf525e4b87b16f548645549159e5048
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2023-12-27 16:38:42 -0300

    Final

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -9,3 +9,23 @@ export const getPosts = async () => {
 
     return []
 }
+
+export const getPostBySlug = async (id) => {
+
+try {
+    const { data } = await api.get(`/posts?id=eq.${id}`);
+
+    if (data && data.length > 0) {
+        // Se houver dados, retorne o primeiro item encontrado (supondo que o ID seja único)
+        return data[0];
+    } else {
+        // Se nenhum post for encontrado com o ID fornecido
+        return null;
+    }
+} catch (error) {
+    // Tratar erros, como conexão perdida, etc.
+    console.error('Erro ao buscar o post por ID:', error);
+    throw error;
+}
+
+}
